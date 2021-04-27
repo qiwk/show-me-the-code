b6ad49a504cb935c25ec3dcfa5334918f53d3bc0
qiwk <qiwk916@163.com>
2021-04-27 18:37:25 +0800

    0002 done

--- num2/num2.py
+++ num2/num2.py
@@ -4,13 +4,16 @@ import random, string
 forselect = string.ascii_letters + '0123456789'
 
 def generate(count, length):
-
+    code = []
     for x in range(count):
-        result = ''
+        result = ""
         for y in range(length):
             result += random.choice(forselect)
+        code.append(result)
+    return code
 
-        print(result)
+code = generate(200, 20)
 
 if __name__ == '__main__':
-    generate(200, 20)
+    print(code)
+
--- nums/num4.py
+++ nums/num4.py
@@ -3,3 +3,26 @@
 作者：Administrator
 日期：2021年04月26日
 """
+import pymysql
+#import sys
+#sys.path.append(r"E:\PycharmProjects\git-projects\show-me-the-code\num2")
+from num2 import num2
+
+mydb = pymysql.connect(host="localhost", user="root", password="970916")
+mycursor = mydb.cursor()
+#mycursor.execute('CREATE DATABASE showcode')
+mycursor.execute("USE showcode")
+mycursor.execute('DROP TABLE IF EXISTS atvcode')
+mycursor.execute('CREATE TABLE atvcode(id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY, code char(50) NOT NULL)')
+# sql = "INSERT INTO atvcode(id, code) VALUES (%s, '%s')" % (1, '125kMac')
+# mycursor.execute(sql)
+
+mycode = num2.code
+for i in range(len(mycode)):
+    #s = mycode[i]
+    mycursor.execute("INSERT INTO atvcode(code) VALUES('%s')" % mycode[i])
+mydb.commit()
+mydb.close()
+
+if __name__ == "__main__":
+    print(mycode)
